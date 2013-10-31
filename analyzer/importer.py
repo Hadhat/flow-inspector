a49387ee266befa15327c0ca0b0a109d564f9ef7
Hadhat <boettget@net.in.tum.de>
2013-10-31 11:30:07 +0100

    Import data from arbitrary tables

--- analyzer/importer.py
+++ analyzer/importer.py
@@ -31,21 +31,39 @@ class FlowBackendImporter(Importer):
 	def __init__(self, last_timestamp = -1):
 		# prepare database connection and create required collection objects
 		self.db = backend.databackend.getBackendObject(config.data_backend, config.data_backend_host, config.data_backend_port, config.data_backend_user, config.data_backend_password, config.data_backend_snmp_name)
-		interface_phy = self.db.getCollection("interface_phy")
+		self.tables = [self.db.getCollection("interface_phy"), self.db.getCollection("ifXTable")]
+		
+		### !!! ### DEBUG ### !!! ### 
+#		last_timestamp = 1377201600
 
 		# get all timestamps
-		self.timestamps = sorted(interface_phy.distinct("timestamp", {"timestamp": {"$gt": last_timestamp}}))
+		self.timestamps = set()
+		for table in self.tables:
+			self.timestamps.update(table.distinct("timestamp", {"timestamp": {"$gt": last_timestamp}}))
+		self.timestamps = sorted(self.timestamps)
 
 	def getNextDataSet(self):
-		interface_phy = self.db.getCollection("interface_phy")
+		
 		timestamp = self.timestamps.pop(0)
 		self.last_timestamp = timestamp
-		db_result = interface_phy.find({"timestamp": timestamp})
 		result = {}
-		for data in db_result:
-			if not data["router"] in result:
-				result[data["router"]] = dict()
-			result[data["router"]][data["ifIndex"]] = data
+
+		for table in self.tables:
+#			db_result = table.find({"timestamp": timestamp, "router": "172.24.10.38", "if_number": "403767312"})
+			db_result = table.find({"timestamp": timestamp})
+			for data in db_result:
+
+				try:
+					result[data["router"]][data["if_number"]].update(data)
+				except:
+					try:
+						result[data["router"]].update({
+							data["if_number"]: data
+						})
+					except:
+						result[data["router"]] = {
+							data["if_number"]: data
+						}
 		return (timestamp, result)
 
 	def __getinitargs__(self):
